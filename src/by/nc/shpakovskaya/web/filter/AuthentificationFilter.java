0e8e04d2f6fe0cc5fdedcd71fcbd2f787f927cc4
1Valeria <shpakovskaya.valeriya.14@gmail.com>
2016-11-28 12:37:50 +0300

    Errors fixed

--- src/by/nc/shpakovskaya/web/filter/AuthentificationFilter.java
+++ src/by/nc/shpakovskaya/web/filter/AuthentificationFilter.java
@@ -2,44 +2,29 @@ package by.nc.shpakovskaya.web.filter;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
  * Created by Valeria on 20.11.2016.
  */
-
-@WebFilter("/controller?command=main")
+@WebFilter( urlPatterns = { "/jsps" },
+        initParams = { @WebInitParam(name = "INDEX_PATH", value = "index.jsp") })
 public class AuthentificationFilter implements Filter {
-
-        @Override
-        public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws ServletException, IOException {
-            HttpServletRequest request = (HttpServletRequest) req;
-            HttpServletResponse response = (HttpServletResponse) res;
-            HttpSession session = request.getSession(false);
-            String loginURI = request.getContextPath() + "/controller?command=enter";
-
-            boolean loggedIn = session != null && session.getAttribute("someone") != null;
-            boolean loginRequest = request.getRequestURI().equals(loginURI);
-
-            if (loggedIn || loginRequest) {
-                System.out.println("+++yes");
-                chain.doFilter(request, response);
-            } else {
-                System.out.println("++no");
-                response.sendRedirect(loginURI);
-            }
-        }
-
-    @Override
-    public void destroy() {
-
+    private String indexPath;
+    public void init(FilterConfig fConfig) throws ServletException {
+        indexPath = fConfig.getInitParameter("INDEX_PATH");
     }
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        // переход на заданную страницу
+        httpResponse.sendRedirect(httpRequest.getContextPath() + indexPath);
+        chain.doFilter(request, response);
+    }
+    public void destroy() {
     }
 }
